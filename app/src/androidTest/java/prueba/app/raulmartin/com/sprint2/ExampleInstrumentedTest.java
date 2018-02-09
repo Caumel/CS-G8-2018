78d2e9ee6c9bf4e7bee7e51e69c4f4e9e4675521
Jaime Morillo <jaimorile@hotmail.es>
2018-02-09 15:25:53 +0100

    Update Instrumented test Which will execute on an Android device. There was a problem with the content of the test.

--- app/src/androidTest/java/prueba/app/raulmartin/com/sprint2/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/prueba/app/raulmartin/com/sprint2/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("prueba.app.raulmartin.com.sprint2", appContext.getPackageName());
+        assertEquals(4, 2 + 2);
     }
 }
