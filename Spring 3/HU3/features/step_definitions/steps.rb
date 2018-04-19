6add121805e33f645c32d84e643fac427609a7d6
DESKTOP-4JKJA9V\Luisc <luiscaumel@gmail.com>
2018-04-19 14:31:03 +0200

    update HU3 with background, refactor HU and add steps.rb

---
+++ Spring 3/HU3/features/step_definitions/steps.rb
@@ -0,0 +1,27 @@
+Given("the ball") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When("game start") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then("I can move the ball up") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then("I can move the ball down") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then("the ball doesn't move by its own") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then("I can't move the ball to the left") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then("I can't move the ball to the right") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
