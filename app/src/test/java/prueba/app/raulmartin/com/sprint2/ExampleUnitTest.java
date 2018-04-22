afbf42f7ff181c57abd7fccbde96f6885000f8ca
JoseFcoRP <pepejp1234@gmail.com>
2018-04-22 23:04:18 +0200

    añadidos test unitarios para gradle

--- app/src/test/java/prueba/app/raulmartin/com/sprint2/ExampleUnitTest.java
+++ app/src/test/java/prueba/app/raulmartin/com/sprint2/ExampleUnitTest.java
@@ -1,17 +1,41 @@
 package prueba.app.raulmartin.com.sprint2;
 
+
+
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
-/**
- * Example local unit test, which will execute on the development machine (host).
- *
- * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
- */
 public class ExampleUnitTest {
+    Bat bat;
     @Test
-    public void addition_isCorrect() throws Exception {
-        assertEquals(4, 2 + 2);
+    public void stopped_isCorrect() throws Exception {
+        bat=new Bat(100,100);
+        assertTrue(bat.getMovementState() == 0);
     }
+    @Test
+    public void up_isCorrect() throws Exception {
+        bat=new Bat(100,100);
+        bat.setX(630);
+        assertTrue(bat.getX()==630);
+    }
+    @Test
+    public void down_isCorrect() throws Exception {
+        bat=new Bat(500,100);
+        bat.setX(400);
+        assertTrue(bat.getX()==400);
+    }
+    @Test
+    public void right_isCorrect() throws Exception {
+        bat=new Bat(100,100);
+        bat.setY(400);
+        assertTrue(bat.getY()==400);
+    }
+    @Test
+    public void left_isCorrect() throws Exception {
+        bat=new Bat(100,500);
+        bat.setY(300);
+        assertTrue(bat.getY()==300);
+    }
+
 }
